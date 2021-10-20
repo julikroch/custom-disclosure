d1adfa11bd6278c91610aac45f3e5eccbcaa0d6b
Julian krochmal <jkrochmal.ext@andromedalatam.com>
2021-10-20 13:39:21 -0300

    adding usestate hook

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 import faqData from '../json/faq.json'
 
 const Faq = () => {
+
+    const [open, setOpen] = useState(-1)
+
+    const openDisclosure = (i: number) => {
+        if (i === open) {
+            setOpen(-1)
+            return
+        }
+        setOpen(i)
+    }
+
     return (
         <div className='faq__wrapper'>
-            <h2 className="faq__title">FAQ</h2>
+            <h2 className="faq__title">faq</h2>
             {faqData.map((data, i) => (
-                <div className="faq__content" key={i}>
-                    <h4 className="faq__content-question">{data.title}</h4>
+                <div className={`faq__content${open === i ? ' visible' : ''}`}>
+                    <h4
+                        className="faq__content-question"
+                        key={i}
+                        onClick={() => openDisclosure(i)}
+                        id="faq__content-question"
+                    >{data.title} <i className="faq__content-question-arrow fa fa-chevron-down"></i></h4>
+
                     <p className="faq__content-answer">{data.answer}</p>
                 </div>
             ))}
