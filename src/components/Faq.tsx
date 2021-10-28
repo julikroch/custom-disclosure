cbafbaa16c6df5f579054a25b9dfc57a9b99e0bc
julikroch <juliankrochmal@gmail.com>
2021-10-28 12:08:57 -0300

    adding type

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import faqData from '../json/faq.json'
 
+type FaqT = {
+    id: number
+    title: string
+    answer: string
+}
+
 const Faq = () => {
 
     const [open, setOpen] = useState(-1)
@@ -16,14 +22,14 @@ const Faq = () => {
     return (
         <div className='faq__wrapper'>
             <h2 className='faq__title'>faq</h2>
-            {faqData.map((data, i) => (
+            {faqData.map((data: FaqT) => (
                 <div
-                    className={ `faq__content${open === i ? ' visible' : '' }`}
-                    key={i}
+                    className={`faq__content${open === data.id ? ' visible' : ''}`}
+                    key={data.id}
                 >
                     <h4
                         className='faq__content-question'
-                        onClick={() => openDisclosure(i)}
+                        onClick={() => openDisclosure(data.id)}
                         id='faq__content-question'
                     >{data.title} <i className='faq__content-question-arrow fa fa-chevron-down'></i></h4>
                     <p className='faq__content-answer'>{data.answer}</p>
